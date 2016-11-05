b79b2b51705c74f2ca97e122aa189fe12f6333df
hashirsidd <re@hotmail.com>
2016-11-05 16:16:33 +0500

    in this commit we are using that type of method, who can return value outside the method using JavaScript

--- lab_Repeating_array_etc/app.js
+++ lab_Repeating_array_etc/app.js
@@ -27,56 +27,70 @@
 
 // resipe List.
 
-var inger1 = ['egg','salt','sugar'];
-var inger2 = ['water','egg','milk'];
-var inger3 = ['flour','sugar','milk'];
+// var inger1 = ['egg','salt','sugar'];
+// var inger2 = ['water','egg','milk'];
+// var inger3 = ['flour','sugar','milk'];
 
-var fullInger = [];
-check(inger1,inger2,inger3);
+// var fullInger = [];
+// check(inger1,inger2,inger3);
 
-// console.log(fullInger);
-// for(var a = 0; a<inger1)
+// // console.log(fullInger);
+// // for(var a = 0; a<inger1)
 
-function check(ing1,ing2,ing3)
-{
-    // var i=0;
-    // var temp = false;
-    // console.log(arrayName);
-    // for(var a=0;a<arrayName.length;a++)
-    // {
-    //     for(var b=0; b<fullInger.length;b++)
-    //     {
-    //         if(arrayName[a]==fullInger[b])
-    //         {
-    //             temp = true;
-    //             break;
-    //         }
-    //     }
+// function check(ing1,ing2,ing3)
+// {
+//     // var i=0;
+//     // var temp = false;
+//     // console.log(arrayName);
+//     // for(var a=0;a<arrayName.length;a++)
+//     // {
+//     //     for(var b=0; b<fullInger.length;b++)
+//     //     {
+//     //         if(arrayName[a]==fullInger[b])
+//     //         {
+//     //             temp = true;
+//     //             break;
+//     //         }
+//     //     }
             
-    //     if(temp == false)
-    //     {
-    //         fullInger.push(arrayName[a]);
-    //     }
-    // }
+//     //     if(temp == false)
+//     //     {
+//     //         fullInger.push(arrayName[a]);
+//     //     }
+//     // }
 
-    for(var a=0; a<ing1.length;a++){
-        if(fullInger.lastIndexOf(ing1[a]) == -1)
-        {
-            fullInger.push(ing1[a]);
-        }
-    }
-    for(var a=0; a<ing2.length;a++){
-        if(fullInger.lastIndexOf(ing2[a]) == -1)
-        {
-            fullInger.push(ing2[a]);
-        }
-    }
-    for(var a=0; a<ing3.length;a++){
-        if(fullInger.lastIndexOf(ing3[a]) == -1)
-        {
-            fullInger.push(ing3[a]);
-        }   
-    }
-    console.log(fullInger);
+//     for(var a=0; a<ing1.length;a++){
+//         if(fullInger.lastIndexOf(ing1[a]) == -1)
+//         {
+//             fullInger.push(ing1[a]);
+//         }
+//     }
+//     for(var a=0; a<ing2.length;a++){
+//         if(fullInger.lastIndexOf(ing2[a]) == -1)
+//         {
+//             fullInger.push(ing2[a]);
+//         }
+//     }
+//     for(var a=0; a<ing3.length;a++){
+//         if(fullInger.lastIndexOf(ing3[a]) == -1)
+//         {
+//             fullInger.push(ing3[a]);
+//         }   
+//     }
+//     console.log(fullInger);
 
+// }
+
+
+
+
+// Return Methods;
+
+function sqrtMethod(number)
+{
+    var result =0;
+    result = number*number;
+    return result;
 }
+var square = sqrtMethod(9);
+console.log(square);
